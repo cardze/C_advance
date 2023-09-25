47f4ea547031fe070c40d7f0f7e088c65d63e212
cardze <j10108jason@gmail.com>
2023-09-25 19:17:26 +0800

    first commit

---
+++ week_1/src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int iter;
+    int row, col;
+    int mat[10][10]={0};
+    scanf("%d", &iter);
+    for(row=0;row<iter;row++){
+        for(col=0; col<iter; col ++){
+            scanf("%d", &mat[row][col]);
+        }
+    }
+
+    int ret = 1;
+    for(row=0;row<iter;row++){
+        for(col=0; col<iter; col ++){
+            if(mat[row][col]!=mat[col][row]) ret=0;
+            if(ret==0) break;
+        }
+    }
+    if(ret ==0 ) printf("No\n");
+    else printf("Yes\n");
+
+    return 0;
+}
---
+++ week_3/src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define N 4
+
+typedef struct reg
+{
+    int id;
+    int score;
+} tReg;
+
+
+int main(int argc , char ** argv){
+    tReg *stu_ptr;
+    tReg *head;
+    int i;
+    stu_ptr = (tReg*)malloc(sizeof(tReg)*N);
+    head = stu_ptr;
+
+    stu_ptr -> id = 1; stu_ptr -> score = 99;
+    stu_ptr ++;
+    stu_ptr -> id = 2; stu_ptr -> score = 80;
+
+    stu_ptr = head;
+    stu_ptr[2].id = 40; stu_ptr[2].score = 60;
+
+    stu_ptr = head;
+    for(i =0 ; i< N ; i++){
+        printf("id: %d, score:%d\n", stu_ptr->id, stu_ptr->score);
+        stu_ptr++;
+    }
+    free(head);
+    
+    return 0;
+}
